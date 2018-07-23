027cfee71710babe93991de0a48fe1a1d6fba2da
Joseph Schell <JosephSchell510@gmail.com>
2018-07-23 16:39:22 -0700

    Added validation for first/last names, emails and passwords.

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -21,31 +21,73 @@
 		// Validate Functions
 		private function validateUserName($userName)
 		{
-			if(srtlen($userName) > 20 || srtlen($userName) < 4)
+			if(strlen($userName) > 20 || strlen($userName) < 4)
 			{
 				array_push($this->errorArray, "Your username must be 
 					between 4 and 20 characters in length.");
+				return;
 			}
+
+			// TODO: Check if username exists.
 		}
 
 		private function validateFirstName($firstName)
 		{
-			
+			if(strlen($firstName) > 20 || strlen($firstName) < 2)
+			{
+				array_push($this->errorArray, "Your first name must be 
+					between 2 and 20 characters in length.");
+				return;
+			}
 		}
 
 		private function validateLastName($lastName)
 		{
-			
+			if(strlen($lastName) > 20 || strlen($lastName) < 2)
+			{
+				array_push($this->errorArray, "Your last name must be 
+					between 2 and 20 characters in length.");
+				return;
+			}
 		}
 
 		private function validateEmails($email, $email2)
 		{
-			
+			if($email != $email2)
+			{
+				array_push($this->errorArray, "Your emails do not match.");
+				return;
+			}
+
+			if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+			{
+				array_push($this->errorArray, "Email is invalid.");
+				return;
+			}
+
+			// TODO: Check that the user name hasn't already been used.
 		}
 
 		private function validatePasswords($password, $password2)
 		{
-			
+			if($password != $password2)
+			{
+				array_push($this->errorArray, "Your passwords do not match.");
+				return;
+			}
+
+			if(preg_match('/[^A-Za-z0-9]/', $password))
+			{
+				array_push($this->errorArray, "Your password may only contain numbers or letters.");
+				return;
+			}
+
+			if(strlen($password) > 30 || strlen($password) < 6)
+			{
+				array_push($this->errorArray, "Your password must be 
+					between 6 and 30 characters in length.");
+				return;
+			}
 		}
 	}
 ?>

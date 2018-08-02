8486460fa73f8ec006bd126ebc8499b4307da6be
Joseph Schell <JosephSchell510@gmail.com>
2018-08-02 16:43:40 -0700

    Added search and navigation options to nav bar.

--- index.php
+++ index.php
@@ -28,6 +28,26 @@ else
 					<a href="index.php" class="logo">
 						<img src="assets/images/icons/atom.png">
 					</a>
+
+					<div class="group">
+						<div class="navItem">
+							<a href="search.php" class="navItemLink">Search
+								<img src="assets/images/icons/UV/search.png" class="icon" alt="Search">
+							</a>
+						</div>
+					</div>
+
+					<div class="group">
+						<div class="navItem">
+							<a href="browse.php" class="navItemLink">Browse</a>
+						</div>
+						<div class="navItem">
+							<a href="yourMusic.php" class="navItemLink">Your Music</a>
+						</div>
+						<div class="navItem">
+							<a href="profile.php" class="navItemLink">Your Name</a>
+						</div>
+					</div>
 				</nav>
 			</div>
 		</div>

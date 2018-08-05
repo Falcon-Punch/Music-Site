874f770153e088aa455557cfb15afa25366732d3
Joseph Schell <JosephSchell510@gmail.com>
2018-08-05 03:03:02 -0700

    Added javascript class and enabled music to start playing.

--- includes/header.php
+++ includes/header.php
@@ -18,6 +18,8 @@ else
 <head>
 	<title>Music Streaming Site</title>
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="assets/js/script.js"></script>
 </head>
 <body>
 	<div id="mainContainer">
